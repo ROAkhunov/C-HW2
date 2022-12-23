ca11bf7e94defa47c449ca149eea17218f73b89e
Rus <roakhunov@gmail.com>
2022-12-23 21:52:40 +0500

    add task 15

---
+++ task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber<8 && dayNumber>0)
+{
+    if (dayNumber==6 || dayNumber==7)
+    {
+       System.Console.WriteLine("выходной"); 
+    }
+    else System.Console.WriteLine("не выходной");
+}
+else System.Console.WriteLine("Нет дня недели с таким номером");
